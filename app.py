f7a2b6baddbc4a40cb777e22496cf6f63f37bd11
Gabriela <balicas.gb@gmail.com>
2019-02-15 12:02:42 -0800

    setup

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from boggle import Boggle
+from flask import Flask, session, request, render_template, redirect, make_response
+from flask_debugtoolbar import DebugToolbarExtension
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "fuiserfobcai23618sdjahb"
+
+debug = DebugToolbarExtension(app)
 boggle_game = Boggle()
+
+
